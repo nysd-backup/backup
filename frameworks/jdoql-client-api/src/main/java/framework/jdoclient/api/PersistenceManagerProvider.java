58dfad82f0c2e8b0e828d500babd4bf276a1a927
judas-i <naohiro.y@gmail.com>
2011-09-27 21:48:21 +0900

    change

--- frameworks/jdoql-client-api/src/main/java/framework/jdoclient/api/PersistenceManagerProvider.java
+++ frameworks/jdoql-client-api/src/main/java/framework/jdoclient/api/PersistenceManagerProvider.java
@@ -1,5 +1,5 @@
 /**
- * Use is subject to license terms.
+ * Copyright 2011 the original author
  */
 package framework.jdoclient.api;
 
@@ -9,7 +9,7 @@ import javax.jdo.PersistenceManager;
  * パーシステンスマネージャのソース.
  *
  * @author yoshida-n
- * @version	created.
+ * @version 2011/08/31 created.
  */
 public interface PersistenceManagerProvider {
 
