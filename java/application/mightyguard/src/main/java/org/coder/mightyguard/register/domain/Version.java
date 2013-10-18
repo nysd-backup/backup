11d6deeab73b1883e50c7cdff4dbf31d244a5fc3
judas-i <naohiro.y@gmail.com>
2013-10-18 21:08:56 +0900

    a

---
+++ java/application/mightyguard/src/main/java/org/coder/mightyguard/register/domain/Version.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package org.coder.mightyguard.register.domain;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author yoshida-n
+ *
+ */
+@XmlRootElement
+public class Version {
+
+	public String version;
+	
+	public String date;
+}
--- java/application/mightyguard/src/main/java/org/coder/mightyguard/register/service/ReferenceService.java
+++ java/application/mightyguard/src/main/java/org/coder/mightyguard/register/service/ReferenceService.java
@@ -3,6 +3,7 @@
  */
 package org.coder.mightyguard.register.service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,6 +19,7 @@ import javax.ws.rs.QueryParam;
 
 import org.coder.gear.query.QueryFactoryFinder;
 import org.coder.gear.query.criteria.query.ListReadQuery;
+import org.coder.mightyguard.register.domain.Version;
 import org.coder.mightyguard.register.domain.application.AppVersion;
 
 /**
@@ -30,6 +32,33 @@ public class ReferenceService {
 	private EntityManagerFactory factory = Persistence.createEntityManagerFactory("mightyguard");
 	
 	private static final QueryFactoryFinder finder = new QueryFactoryFinder();
+	
+	/**
+	 * Gets the version list
+	 * @param version
+	 * @return
+	 */
+	@Path("list")
+	@GET
+	@Produces("application/json")
+	public List<Version> previousList(@QueryParam("current") String version){
+		QueryFactoryFinder finder = new QueryFactoryFinder();
+		EntityManager em = factory.createEntityManager();
+		ListReadQuery<AppVersion> currentQuery = finder.createCriteriaQueryFactory().createListReadQuery(AppVersion.class, em);
+		List<AppVersion> cList = currentQuery.lt("version", version).call();
+		Map<String,String> tree = new TreeMap<String,String>();
+		for(AppVersion v : cList){
+			tree.put(v.date, v.version);
+		}
+		List<Version> vlist = new ArrayList<Version>();
+		for(Map.Entry<String, String> v : tree.entrySet()){
+			Version e = new Version();
+			e.date = v.getKey();
+			e.version = v.getValue();
+			vlist.add(e);
+		}
+		return vlist;
+	}
 
 	/**
 	 * @param version
