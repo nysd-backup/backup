057f7a116e109a31738209e866900258862a85b7
judas-i <naohiro.y@gmail.com>
2014-01-16 22:29:56 +0900

    s

---
+++ java/sample/sastruts/src/main/java/org/coder/gear/sample/sastruts/dto/OrderForm.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.coder.gear.sample.sastruts.dto;
+
+import org.coder.gear.sample.sastruts.entity.Order;
+
+/**
+ * @author yoshida-n
+ *
+ */
+public class OrderForm {
+
+	public Order order;
+}
